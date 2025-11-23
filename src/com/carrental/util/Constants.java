84cbdc77597de4bfa5a3ae8b564056ace4e47d56
郑在希 <12240093+zheng-zaixi@user.noreply.gitee.com>
2025-11-23 14:07:18 +0800

    实现常量类

---
+++ src/com/carrental/util/Constants.java
@@ -0,0 +1,22 @@
+package com.carrental.util;
+
+/**
+ * 常量类
+ */
+public class Constants {
+    // 客户类型
+    public static final String CUSTOMER_TYPE_REGULAR = "普通";
+    public static final String CUSTOMER_TYPE_VIP = "VIP";
+    public static final String CUSTOMER_TYPE_ENTERPRISE = "企业";
+
+    // 车辆类型
+    public static final String VEHICLE_TYPE_BUS = "客车";
+    public static final String VEHICLE_TYPE_CAR = "轿车";
+    public static final String VEHICLE_TYPE_VAN = "面包车";
+
+    // 折扣率
+    public static final double DISCOUNT_REGULAR = 1.0;    // 普通客户无折扣
+    public static final double DISCOUNT_VIP = 0.95;       // VIP客户95折
+    public static final double DISCOUNT_ENTERPRISE = 0.9; // 企业客户9折
+
+}
