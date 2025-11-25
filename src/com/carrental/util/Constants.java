144eddba3ae23badc140150602549a3dc8eefc34
三亦 <2223491990@qq.com>
2025-11-25 15:21:01 +0800

    新增文件路径常量

--- src/com/carrental/util/Constants.java
+++ src/com/carrental/util/Constants.java
@@ -14,6 +14,15 @@ public class Constants {
     public static final String VEHICLE_TYPE_CAR = "轿车";
     public static final String VEHICLE_TYPE_VAN = "面包车";
 
+    // 车辆数据文件路径
+    public static final String VEHICLE_FILE_PATH = "./data/vehicles.txt";
+    // 用户数据文件路径
+    public static final String USER_FILE_PATH = "./data/users.txt";
+    // 租赁记录文件路径
+    public static final String RENTAL_FILE_PATH = "./data/rental_records.txt";
+    // 客户数据文件路径
+    public static final String CUSTOMER_FILE_PATH = "./data/customers.txt";
+
     // 折扣率
     public static final double DISCOUNT_REGULAR = 1.0;    // 普通客户无折扣
     public static final double DISCOUNT_VIP = 0.95;       // VIP客户95折
